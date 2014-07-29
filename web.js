79b8fa11a1ae4f5cae700f6669fd6c92939f5b95
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-07-29 04:24:26 -0500

    web.js ok7!

--- web.js
+++ web.js
@@ -1,22 +1,7 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var gzippo = require('gzippo');
+var express = require('express');
 var app = express();
 
-var gzippo = require('gzippo');
-
-app.use(logfmt.requestLogger());
-
-
+app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-/*
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});*/
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
-
-
+app.listen(process.env.PORT || 5000);
