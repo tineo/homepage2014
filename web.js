e5d820ca54ab91c69fea57e6612588836b998a5b
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-07-29 03:53:57 -0500

    Go!

--- web.js
+++ web.js
@@ -1,7 +1,15 @@
-var gzippo = require('gzippo');
-var express = require('express');
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
