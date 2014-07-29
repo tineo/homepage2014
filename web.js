a64d347c36deaf9cd6123f84f0646c6bd3dfa617
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-07-29 04:42:54 -0500

    web.js wee!

--- web.js
+++ web.js
@@ -1,54 +1,14 @@
-var app, express, oneDay, pushState;
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
 
-express = require("express");
 
-app = express();
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-console.log(app.settings.env);
-
-oneDay = 86400000;
-
-pushState = function(req, res) {
-    var newUrl;
-    newUrl = req.protocol + '://' + req.get('Host') + '/#' + req.url;
-    return res.redirect(newUrl);
-};
-
-app.configure('development', function() {
-    app.use(express.compress());
-    app.use(express["static"](__dirname + '/dist'));
-    return app.use(express.logger());
+app.use(app.router);
+app.use(function(req, res) {
+    // Use res.sendfile, as it streams instead of reading the file into memory.
+    res.sendfile(__dirname + '/dist/index.html');
 });
 
-app.configure('staging', function() {
-    app.use(express.basicAuth('whateveruser', 'whateverpassword'));
-    app.use(express.compress());
-    return app.use(express["static"](__dirname + '/dist'));
-});
-
-app.configure('production', function() {
-    app.use(express.compress());
-    return app.use(express["static"](__dirname + '/dist', {
-        maxAge: oneDay
-    }));
-});
-
-app.get('*', function(req, res) {
-    var newUrl;
-    newUrl = req.protocol + '://' + req.get('Host') + '/#' + req.url;
-    return res.redirect(newUrl);
-});
-
-exports.startServer = function(port, path, callback) {
-    var p;
-    p = process.env.PORT || port;
-    console.log("Starting server on port: " + p + ", path /" + path);
-    app.listen(p);
-    if (callback != null) {
-        return callback(app);
-    }
-};
-
-if (process.env.PORT) {
-    this.startServer(process.env.PORT, "dist");
-}
+app.listen(process.env.PORT || 5000);
