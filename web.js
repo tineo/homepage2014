79b89e0ead9144fd8ab67becda8c1b031d778bb8
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-07-29 04:45:05 -0500

    web.js statics!

--- web.js
+++ web.js
@@ -5,10 +5,11 @@ var app = express();
 
 //app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-app.use(app.router);
+/*app.use(app.router);
 app.use(function(req, res) {
-    // Use res.sendfile, as it streams instead of reading the file into memory.
     res.sendfile(__dirname + '/dist/index.html');
-});
+});*/
+
+app.use(express.static(__dirname + '/dist/index.html'));
 
 app.listen(process.env.PORT || 5000);
