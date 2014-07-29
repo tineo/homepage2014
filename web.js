48874db7f6602dcf77d477e5ab720d1aa34d275b
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2014-07-29 04:57:00 -0500

    web.js staticgit config --global push.default current

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist/index.html"));
 
 /*app.use(app.router);
 app.use(function(req, res) {
     res.sendfile(__dirname + '/dist/index.html');
 });*/
 
-app.use(express.static(__dirname + '/index.html'));
+//app.use(express.static(__dirname + '/index.html'));
 
 app.listen(process.env.PORT || 5000);
